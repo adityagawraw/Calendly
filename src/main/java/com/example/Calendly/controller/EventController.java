d55ebef0bedd9e7fbc5d1d9c8851d6902d66af90
rajendra <rajumahapatra096@gmail.com>
2023-12-02 17:41:55 +0530

    Dashboard page functionality created

--- src/main/java/com/example/Calendly/controller/EventController.java
+++ src/main/java/com/example/Calendly/controller/EventController.java
@@ -1,5 +1,6 @@
 package com.example.Calendly.controller;
 
+import com.example.Calendly.model.Event;
 import com.example.Calendly.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,12 +8,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class EventController {
     public final EventService eventService;
     @Autowired
     public EventController(EventService eventService) {
         this.eventService = eventService;
+    }
+
+    @GetMapping("/dashboard")
+    public String showDashboard(Model model) {
+        List<Event> events = eventService.findAllEvents();
+        model.addAttribute("events", events);
+
+        return "dashboard";
+
     }
 
     @GetMapping("/create-event")
