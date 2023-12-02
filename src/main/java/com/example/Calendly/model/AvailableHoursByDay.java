ddb49fa8cbd3adf5f587898431631f34e6566dad
rajendra <rajumahapatra096@gmail.com>
2023-12-02 14:21:42 +0530

    Database setup done and required models are created

--- src/main/java/com/example/Calendly/model/AvailableHoursByDay.java
+++ src/main/java/com/example/Calendly/model/AvailableHoursByDay.java
@@ -1,4 +1,33 @@
 package com.example.Calendly.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "available_hour_by_day")
 public class AvailableHoursByDay {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "day")
+    private String day;
+
+    @Column(name = "start")
+    private Integer start;
+
+    @Column(name = "end")
+    private Integer end;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
 }
--- src/main/java/com/example/Calendly/model/Event.java
+++ src/main/java/com/example/Calendly/model/Event.java
@@ -1,26 +1,61 @@
 package com.example.Calendly.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
-import java.util.Map;
 
 @Entity
-@Table(name = "entity")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "event")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "host")
     private final String host ="aditya";
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "duration")
     private int duration;
+
+    @Column(name = "location")
     private String location;
+
+    @Column(name = "event_color")
     private String eventColor;
+
+    @Column(name = "date_range")
     private int dateRange;
+
+    @OneToMany(mappedBy = "event")
     private List<AvailableHoursByDay> availableHoursByDays;
+
+    @Column(name = "limit_per_day")
     private int limitPerDay;
+
+    @Column(name = "invitee_name")
     private String inviteeName;
+
+    @Column(name = "invitee_email")
     private String inviteeEmail;
-    private List<String> meetQuestions;
+
+    @OneToMany(mappedBy = "event")
+    private List<EventQuestion> meetQuestions;
+
+    @Column(name = "email_confirmation")
     private boolean emailConfirmation;
 }
