f0a45fa39b45cdd75a457a656f739eae623aadbb
aditya <adityagawraw006@gmail.com>
2023-12-02 12:35:08 +0530

    create event page

---
+++ src/main/java/com/example/Calendly/controller/EventController.java
@@ -0,0 +1,13 @@
+package com.example.Calendly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EventController {
+
+    @GetMapping("/create-event")
+    public  String getCreateEventPage(){
+        return "creat-event";
+    }
+}
